41a531cb99f85588af6f28c16605bbc488b3493d
aeiowoo <aeiowoo@gmail.com>
2016-11-26 22:56:27 +0900

    Initial Commit

---
+++ projectTrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectTrip
+//
+//  Created by Jeon Myeong Woo on 2016. 11. 26..
+//  Copyright © 2016년 SwiftTive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
