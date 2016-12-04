ac2eb720d8766051da0839998babad78d88a72ab
aeiowoo <aeiowoo@gmail.com>
2016-12-04 12:02:25 +0900

    전명우 - 환율 API 테스트 코드

--- projectTrip/ViewController.swift
+++ projectTrip/ViewController.swift
@@ -15,6 +15,33 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
         // TEST
         debugPrint("aaaa")
+		
+		//환율 테스트
+		let urlPath: String = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22USDKRW%22&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+		let url: NSURL = NSURL(string: urlPath)!
+		let request1: NSURLRequest = NSURLRequest(url: url as URL)
+		let response: AutoreleasingUnsafeMutablePointer<URLResponse?>?=nil
+		
+		
+		do{
+			
+			let dataVal = try NSURLConnection.sendSynchronousRequest(request1 as URLRequest, returning: response)
+			
+			print(response)
+			do {
+				if let jsonResult = try JSONSerialization.jsonObject(with: dataVal, options: []) as? NSDictionary {
+					print("Synchronous\(jsonResult)")
+				}
+			} catch let error as NSError {
+				print(error.localizedDescription)
+			}
+			
+			
+			
+		}catch let error as NSError
+		{
+			print(error.localizedDescription)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
