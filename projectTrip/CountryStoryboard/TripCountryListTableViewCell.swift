d76487f700cb470eaf78a1a5751cf38c67b1a376
Daniel <Daniel@Yeonhos-MacBook-Pro-2.local>
2017-01-22 11:17:16 +0900

    - Updating Country List

---
+++ projectTrip/CountryStoryboard/TripCountryListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TripCountryListTableViewCell.swift
+//  projectTrip
+//
+//  Created by Magic Dan on 22/01/2017.
+//  Copyright © 2017 SwiftTive. All rights reserved.
+//
+
+import UIKit
+
+class TripCountryListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
